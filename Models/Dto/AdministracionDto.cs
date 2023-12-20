aef5226e71f6b958f75e9f17a334fdb4cb59cc98
@maquinaDeChelo <msd@gmail.com>
2023-12-20 03:26:33 -0300

    segundo commit

---
+++ Models/Dto/AdministracionDto.cs
@@ -0,0 +1,21 @@
+﻿namespace SisacadFinal.Models.Dto
+{
+    public class AdministracionDto
+    {
+     
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Rol { get; set; }
+
+        public int? Dni { get; set; }
+
+        public string? Correo { get; set; }
+
+   
+
+        public DateTime? FechaCreacion { get; set; }
+    }
+}
---
+++ Profiles/AllProfiles.cs
@@ -0,0 +1,19 @@
+﻿using SisacadFinal.Models.Dto;
+using SisacadFinal.Models;
+using AutoMapper;
+
+namespace SisacadFinal.Profiles
+{
+    public class AllProfiles:Profile
+    {
+        public AllProfiles()
+        {
+            CreateMap<Estudiantes, EstudiantesDto>();
+            CreateMap<Profesores, ProfesoresDto>();
+            CreateMap<ProfesoresDto, Profesores>();
+            CreateMap < AdministracionDto, Administracion>();
+            CreateMap<Administracion, AdministracionDto>();
+            // Agrega más mapeos según sea necesario
+        }
+    }
+}
